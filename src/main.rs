ea259fe9bf47c46bbc7ea22d4494687f2c264f7f
mhorrach <marti.horrachvicens@metro-markets.de>
2025-02-03 14:04:35 +0100

    add bayer dithering

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,28 @@ use opencv::{
 use std::net::TcpListener;
 use std::io::Write;
 
+fn bayer_dithering(frame: &Mat, bayer_matrix: &[u8; 16]) -> Mat {
+    let mut dithered_frame = Mat::default();
+    frame.copy_to(&mut dithered_frame).unwrap();
+
+    let rows = dithered_frame.rows();
+    let cols = dithered_frame.cols();
+
+    for i in 0..rows {
+        for j in 0..cols {
+            let pixel = dithered_frame.at_2d::<u8>(i, j).unwrap();
+            let bayer_value = bayer_matrix[((i % 4) * 4 + (j % 4)) as usize];
+
+            let scaled_bayer_value = (bayer_value as f32 / 16.0 * 255.0) as u8;
+
+            let new_pixel = if *pixel > scaled_bayer_value { 255 } else { 0 };
+            *dithered_frame.at_2d_mut::<u8>(i, j).unwrap() = new_pixel;
+        }
+    }
+
+    dithered_frame
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     println!("Server listening on port 8080");
@@ -16,6 +38,14 @@ fn main() {
     let mut frame = Mat::default();
     let mut gray_frame = Mat::default();
     let mut buf = Vector::new();
+
+    // 4x4 Bayer matrix
+    let bayer_matrix: [u8; 16] = [
+        0, 8, 2, 10,
+        12, 4, 14, 6,
+        3, 11, 1, 9,
+        15, 7, 13, 5,
+    ];
 
     loop {
         let (mut stream, _) = listener.accept().expect("Failed to accept connection");
@@ -35,9 +65,10 @@ fn main() {
 
             imgproc::cvt_color(&frame, &mut gray_frame, imgproc::COLOR_BGR2GRAY, 0).unwrap();
 
+            let dithered_frame = bayer_dithering(&gray_frame, &bayer_matrix);
 
             buf.clear();
-            let _ = imgcodecs::imencode(".jpg", &gray_frame, &mut buf, &Vector::new());
+            let _ = imgcodecs::imencode(".jpg", &dithered_frame, &mut buf, &Vector::new());
 
             let image_data = format!(
                 "--frame\r\nContent-Type: image/jpeg\r\nContent-Length: {}\r\n\r\n",
